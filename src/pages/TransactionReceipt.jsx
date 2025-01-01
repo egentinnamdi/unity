0ce8d2eeb285b519e11f3aba9239c872f40bce60
Nnamdi James <egentinnamdi10@gmail.com>
2025-01-01 01:08:50 +0100

    Update TransactionReceipt.jsx

--- src/pages/TransactionReceipt.jsx
+++ src/pages/TransactionReceipt.jsx
@@ -69,7 +69,7 @@ function TransactionReceipt() {
           transaction receipt
         </Typography>
         <Typography className="text-right !font-medium" component="span">
-          created at: {new Date(details?.createdAt || null).toDateTimeString()}
+          created at: {new Date(details?.createdAt || null).toIsoString()}
         </Typography>
       </Box>
       <Container
